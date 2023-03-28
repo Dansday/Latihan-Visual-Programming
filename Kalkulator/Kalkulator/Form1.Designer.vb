03cae5601a9f3d3947dab2915cfd4d9eaab102a2
Akbar Yudhanto <50676898+Dansday@users.noreply.github.com>
2023-03-29 00:21:47 +0800

    Add files via upload

---
+++ Kalkulator/Kalkulator/Form1.Designer.vb
@@ -0,0 +1,278 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Layar = New System.Windows.Forms.TextBox()
+        Me.N1 = New System.Windows.Forms.Button()
+        Me.N2 = New System.Windows.Forms.Button()
+        Me.N3 = New System.Windows.Forms.Button()
+        Me.N4 = New System.Windows.Forms.Button()
+        Me.N5 = New System.Windows.Forms.Button()
+        Me.N6 = New System.Windows.Forms.Button()
+        Me.Kurang = New System.Windows.Forms.Button()
+        Me.N7 = New System.Windows.Forms.Button()
+        Me.N8 = New System.Windows.Forms.Button()
+        Me.N9 = New System.Windows.Forms.Button()
+        Me.Kali = New System.Windows.Forms.Button()
+        Me.Total = New System.Windows.Forms.Button()
+        Me.N0 = New System.Windows.Forms.Button()
+        Me.Clear = New System.Windows.Forms.Button()
+        Me.Bagi = New System.Windows.Forms.Button()
+        Me.Tambah = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Layar
+        '
+        Me.Layar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Layar.Location = New System.Drawing.Point(13, 27)
+        Me.Layar.Name = "Layar"
+        Me.Layar.ReadOnly = True
+        Me.Layar.Size = New System.Drawing.Size(179, 26)
+        Me.Layar.TabIndex = 0
+        Me.Layar.Text = "0"
+        Me.Layar.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'N1
+        '
+        Me.N1.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.N1.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.N1.Location = New System.Drawing.Point(13, 70)
+        Me.N1.Name = "N1"
+        Me.N1.Size = New System.Drawing.Size(40, 40)
+        Me.N1.TabIndex = 1
+        Me.N1.Text = "1"
+        Me.N1.UseVisualStyleBackColor = False
+        '
+        'N2
+        '
+        Me.N2.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.N2.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.N2.Location = New System.Drawing.Point(59, 70)
+        Me.N2.Name = "N2"
+        Me.N2.Size = New System.Drawing.Size(40, 40)
+        Me.N2.TabIndex = 2
+        Me.N2.Text = "2"
+        Me.N2.UseVisualStyleBackColor = False
+        '
+        'N3
+        '
+        Me.N3.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.N3.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.N3.Location = New System.Drawing.Point(105, 70)
+        Me.N3.Name = "N3"
+        Me.N3.Size = New System.Drawing.Size(40, 40)
+        Me.N3.TabIndex = 3
+        Me.N3.Text = "3"
+        Me.N3.UseVisualStyleBackColor = False
+        '
+        'N4
+        '
+        Me.N4.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.N4.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.N4.Location = New System.Drawing.Point(13, 116)
+        Me.N4.Name = "N4"
+        Me.N4.Size = New System.Drawing.Size(40, 40)
+        Me.N4.TabIndex = 5
+        Me.N4.Text = "4"
+        Me.N4.UseVisualStyleBackColor = False
+        '
+        'N5
+        '
+        Me.N5.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.N5.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.N5.Location = New System.Drawing.Point(59, 116)
+        Me.N5.Name = "N5"
+        Me.N5.Size = New System.Drawing.Size(40, 40)
+        Me.N5.TabIndex = 6
+        Me.N5.Text = "5"
+        Me.N5.UseVisualStyleBackColor = False
+        '
+        'N6
+        '
+        Me.N6.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.N6.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.N6.Location = New System.Drawing.Point(105, 116)
+        Me.N6.Name = "N6"
+        Me.N6.Size = New System.Drawing.Size(40, 40)
+        Me.N6.TabIndex = 7
+        Me.N6.Text = "6"
+        Me.N6.UseVisualStyleBackColor = False
+        '
+        'Kurang
+        '
+        Me.Kurang.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Kurang.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Kurang.Location = New System.Drawing.Point(151, 116)
+        Me.Kurang.Name = "Kurang"
+        Me.Kurang.Size = New System.Drawing.Size(40, 40)
+        Me.Kurang.TabIndex = 8
+        Me.Kurang.Text = "-"
+        Me.Kurang.UseVisualStyleBackColor = False
+        '
+        'N7
+        '
+        Me.N7.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.N7.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.N7.Location = New System.Drawing.Point(13, 162)
+        Me.N7.Name = "N7"
+        Me.N7.Size = New System.Drawing.Size(40, 40)
+        Me.N7.TabIndex = 9
+        Me.N7.Text = "7"
+        Me.N7.UseVisualStyleBackColor = False
+        '
+        'N8
+        '
+        Me.N8.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.N8.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.N8.Location = New System.Drawing.Point(59, 162)
+        Me.N8.Name = "N8"
+        Me.N8.Size = New System.Drawing.Size(40, 40)
+        Me.N8.TabIndex = 10
+        Me.N8.Text = "8"
+        Me.N8.UseVisualStyleBackColor = False
+        '
+        'N9
+        '
+        Me.N9.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.N9.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.N9.Location = New System.Drawing.Point(105, 162)
+        Me.N9.Name = "N9"
+        Me.N9.Size = New System.Drawing.Size(40, 40)
+        Me.N9.TabIndex = 11
+        Me.N9.Text = "9"
+        Me.N9.UseVisualStyleBackColor = False
+        '
+        'Kali
+        '
+        Me.Kali.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Kali.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Kali.Location = New System.Drawing.Point(151, 162)
+        Me.Kali.Name = "Kali"
+        Me.Kali.Size = New System.Drawing.Size(40, 40)
+        Me.Kali.TabIndex = 12
+        Me.Kali.Text = "*"
+        Me.Kali.UseVisualStyleBackColor = False
+        '
+        'Total
+        '
+        Me.Total.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Total.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Total.Location = New System.Drawing.Point(13, 207)
+        Me.Total.Name = "Total"
+        Me.Total.Size = New System.Drawing.Size(40, 40)
+        Me.Total.TabIndex = 13
+        Me.Total.Text = "="
+        Me.Total.UseVisualStyleBackColor = False
+        '
+        'N0
+        '
+        Me.N0.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.N0.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.N0.Location = New System.Drawing.Point(59, 208)
+        Me.N0.Name = "N0"
+        Me.N0.Size = New System.Drawing.Size(40, 40)
+        Me.N0.TabIndex = 14
+        Me.N0.Text = "0"
+        Me.N0.UseVisualStyleBackColor = False
+        '
+        'Clear
+        '
+        Me.Clear.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Clear.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Clear.Location = New System.Drawing.Point(105, 208)
+        Me.Clear.Name = "Clear"
+        Me.Clear.Size = New System.Drawing.Size(40, 40)
+        Me.Clear.TabIndex = 15
+        Me.Clear.Text = "C"
+        Me.Clear.UseVisualStyleBackColor = False
+        '
+        'Bagi
+        '
+        Me.Bagi.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Bagi.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Bagi.Location = New System.Drawing.Point(151, 208)
+        Me.Bagi.Name = "Bagi"
+        Me.Bagi.Size = New System.Drawing.Size(40, 40)
+        Me.Bagi.TabIndex = 16
+        Me.Bagi.Text = "/"
+        Me.Bagi.UseVisualStyleBackColor = False
+        '
+        'Tambah
+        '
+        Me.Tambah.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Tambah.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Tambah.Location = New System.Drawing.Point(151, 70)
+        Me.Tambah.Name = "Tambah"
+        Me.Tambah.Size = New System.Drawing.Size(40, 40)
+        Me.Tambah.TabIndex = 17
+        Me.Tambah.Text = "+"
+        Me.Tambah.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(202, 259)
+        Me.Controls.Add(Me.Tambah)
+        Me.Controls.Add(Me.Bagi)
+        Me.Controls.Add(Me.Clear)
+        Me.Controls.Add(Me.N0)
+        Me.Controls.Add(Me.Total)
+        Me.Controls.Add(Me.Kali)
+        Me.Controls.Add(Me.N9)
+        Me.Controls.Add(Me.N8)
+        Me.Controls.Add(Me.N7)
+        Me.Controls.Add(Me.Kurang)
+        Me.Controls.Add(Me.N6)
+        Me.Controls.Add(Me.N5)
+        Me.Controls.Add(Me.N4)
+        Me.Controls.Add(Me.N3)
+        Me.Controls.Add(Me.N2)
+        Me.Controls.Add(Me.N1)
+        Me.Controls.Add(Me.Layar)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.Text = "Calcu 028"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Layar As TextBox
+    Friend WithEvents N1 As Button
+    Friend WithEvents N2 As Button
+    Friend WithEvents N3 As Button
+    Friend WithEvents N4 As Button
+    Friend WithEvents N5 As Button
+    Friend WithEvents N6 As Button
+    Friend WithEvents Kurang As Button
+    Friend WithEvents N7 As Button
+    Friend WithEvents N8 As Button
+    Friend WithEvents N9 As Button
+    Friend WithEvents Kali As Button
+    Friend WithEvents Total As Button
+    Friend WithEvents N0 As Button
+    Friend WithEvents Clear As Button
+    Friend WithEvents Bagi As Button
+    Friend WithEvents Tambah As Button
+End Class
---
+++ Kalkulator/Kalkulator/Form1.vb
@@ -0,0 +1,142 @@
+﻿Public Class Form1
+
+    Dim NoAwal As Long
+    Dim Operation As String
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles Layar.TextChanged
+
+    End Sub
+
+    Private Sub N1_Click(sender As Object, e As EventArgs) Handles N1.Click
+        If Layar.Text = "0" Then
+            Layar.Text = "1"
+        Else
+            Layar.Text = Layar.Text & "1"
+        End If
+    End Sub
+
+    Private Sub N2_Click(sender As Object, e As EventArgs) Handles N2.Click
+        If Layar.Text = "0" Then
+            Layar.Text = "2"
+        Else
+            Layar.Text = Layar.Text & "2"
+        End If
+    End Sub
+
+    Private Sub N3_Click(sender As Object, e As EventArgs) Handles N3.Click
+        If Layar.Text = "0" Then
+            Layar.Text = "3"
+        Else
+            Layar.Text = Layar.Text & "3"
+        End If
+    End Sub
+
+    Private Sub N4_Click(sender As Object, e As EventArgs) Handles N4.Click
+        If Layar.Text = "0" Then
+            Layar.Text = "4"
+        Else
+            Layar.Text = Layar.Text & "4"
+        End If
+    End Sub
+
+    Private Sub N5_Click(sender As Object, e As EventArgs) Handles N5.Click
+        If Layar.Text = "0" Then
+            Layar.Text = "5"
+        Else
+            Layar.Text = Layar.Text & "5"
+        End If
+    End Sub
+
+    Private Sub N6_Click(sender As Object, e As EventArgs) Handles N6.Click
+        If Layar.Text = "0" Then
+            Layar.Text = "6"
+        Else
+            Layar.Text = Layar.Text & "6"
+        End If
+    End Sub
+
+    Private Sub N7_Click(sender As Object, e As EventArgs) Handles N7.Click
+        If Layar.Text = "0" Then
+            Layar.Text = "7"
+        Else
+            Layar.Text = Layar.Text & "7"
+        End If
+    End Sub
+
+    Private Sub N8_Click(sender As Object, e As EventArgs) Handles N8.Click
+        If Layar.Text = "0" Then
+            Layar.Text = "8"
+        Else
+            Layar.Text = Layar.Text & "8"
+        End If
+    End Sub
+
+    Private Sub N9_Click(sender As Object, e As EventArgs) Handles N9.Click
+        If Layar.Text = "0" Then
+            Layar.Text = "9"
+        Else
+            Layar.Text = Layar.Text & "9"
+        End If
+    End Sub
+
+    Private Sub N0_Click(sender As Object, e As EventArgs) Handles N0.Click
+        If Layar.Text = "0" Then
+            Layar.Text = "0"
+        Else
+            Layar.Text = Layar.Text & "0"
+        End If
+    End Sub
+
+    Private Sub Clear_Click(sender As Object, e As EventArgs) Handles Clear.Click
+        Layar.Text = "0"
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Tambah_Click(sender As Object, e As EventArgs) Handles Tambah.Click
+        NoAwal = Layar.Text
+        Layar.Text = "0"
+        Operation = "+"
+    End Sub
+
+    Private Sub Kurang_Click(sender As Object, e As EventArgs) Handles Kurang.Click
+        NoAwal = Layar.Text
+        Layar.Text = "0"
+        Operation = "-"
+    End Sub
+
+    Private Sub Kali_Click(sender As Object, e As EventArgs) Handles Kali.Click
+        NoAwal = Layar.Text
+        Layar.Text = "0"
+        Operation = "*"
+    End Sub
+
+    Private Sub Bagi_Click(sender As Object, e As EventArgs) Handles Bagi.Click
+        NoAwal = Layar.Text
+        Layar.Text = "0"
+        Operation = "/"
+    End Sub
+
+    Private Sub Total_Click(sender As Object, e As EventArgs) Handles Total.Click
+        Dim NoKedua As Long
+        Dim Result As Long
+
+        NoKedua = Layar.Text
+
+        If Operation = "+" Then
+            Result = NoAwal + NoKedua
+        ElseIf Operation = "-" Then
+            Result = NoAwal - NoKedua
+        ElseIf Operation = "*" Then
+            Result = NoAwal * NoKedua
+        ElseIf Operation = "/" Then
+            Result = NoAwal / NoKedua
+        End If
+
+        NoAwal = Result
+        Layar.Text = Result
+
+    End Sub
+End Class
